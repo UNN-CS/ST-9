cbda6a5e6d8ea0b9a3eff24691acb755d3a9ad2d
vitaliivoroshilov <vitallizer@yandex.ru>
2025-05-28 01:36:40 +0300

    st-9 init

---
+++ TestProject1/Test1.cs
@@ -0,0 +1,136 @@
+﻿using OpenQA.Selenium.Appium;
+using OpenQA.Selenium.Appium.Windows;
+
+namespace TestProject1
+{
+    [TestClass]
+    [DoNotParallelize]
+    public sealed class Test1
+    {
+        public const string DriveURL = "http://127.0.0.1:4723/";
+        public const string AppPath = @"C:\Main\Dev\ST\ST-9\src\bin\Debug\net8.0-windows\HotelCalculator.exe";
+        public WindowsDriver<WindowsElement> driver;
+
+        [TestInitialize]
+        public void Setup()
+        {
+            var appCapabilities = new AppiumOptions();
+            appCapabilities.AddAdditionalCapability("app", AppPath);
+            appCapabilities.AddAdditionalCapability("ms:waitForAppLaunch", "5");
+            appCapabilities.AddAdditionalCapability("deviceName", "WindowsPC");
+
+            driver = new WindowsDriver<WindowsElement>(new Uri(DriveURL), appCapabilities);
+
+            driver.FindElementByAccessibilityId("textBox_days").Clear();
+            driver.FindElementByAccessibilityId("textBox_category").Clear();
+            driver.FindElementByAccessibilityId("textBox_capacity").Clear();
+            driver.FindElementByAccessibilityId("textBox_safe").Clear();
+            driver.FindElementByAccessibilityId("textBox_breakfast").Clear();
+            driver.FindElementByAccessibilityId("textBox_sum").Clear();
+        }
+
+        [TestMethod]
+        public void TestMethod_standard()
+        {
+            driver.FindElementByAccessibilityId("textBox_days").SendKeys("1");
+            driver.FindElementByAccessibilityId("textBox_category").SendKeys("2");
+            driver.FindElementByAccessibilityId("textBox_capacity").SendKeys("3");
+            driver.FindElementByAccessibilityId("textBox_safe").SendKeys("нет");
+            driver.FindElementByAccessibilityId("textBox_breakfast").SendKeys("нет");
+            driver.FindElementByAccessibilityId("button1").Click();
+
+            // 200*3*1 + 0 + 0 = 600
+            var sum = driver.FindElementByAccessibilityId("textBox_sum").Text;
+            Assert.AreEqual("600", sum);
+        }
+
+        [TestMethod]
+        public void TestMethod_safe()
+        {
+            driver.FindElementByAccessibilityId("textBox_days").SendKeys("1");
+            driver.FindElementByAccessibilityId("textBox_category").SendKeys("2");
+            driver.FindElementByAccessibilityId("textBox_capacity").SendKeys("3");
+            driver.FindElementByAccessibilityId("textBox_safe").SendKeys("да");
+            driver.FindElementByAccessibilityId("textBox_breakfast").SendKeys("нет");
+            driver.FindElementByAccessibilityId("button1").Click();
+
+            // 200*3*1 + 10 + 0 = 610
+            var sum = driver.FindElementByAccessibilityId("textBox_sum").Text;
+            Assert.AreEqual("610", sum);
+        }
+
+        [TestMethod]
+        public void TestMethod_breakfast()
+        {
+            driver.FindElementByAccessibilityId("textBox_days").SendKeys("1");
+            driver.FindElementByAccessibilityId("textBox_category").SendKeys("2");
+            driver.FindElementByAccessibilityId("textBox_capacity").SendKeys("3");
+            driver.FindElementByAccessibilityId("textBox_safe").SendKeys("нет");
+            driver.FindElementByAccessibilityId("textBox_breakfast").SendKeys("да");
+            driver.FindElementByAccessibilityId("button1").Click();
+
+            // 200*3*1 + 0 + 20*3*1 = 660
+            var sum = driver.FindElementByAccessibilityId("textBox_sum").Text;
+            Assert.AreEqual("660", sum);
+        }
+        
+        [TestMethod]
+        public void TestMethod_days_incorrect()
+        {
+            driver.FindElementByAccessibilityId("textBox_days").SendKeys("-1");
+            driver.FindElementByAccessibilityId("textBox_category").SendKeys("2");
+            driver.FindElementByAccessibilityId("textBox_capacity").SendKeys("3");
+            driver.FindElementByAccessibilityId("textBox_safe").SendKeys("нет");
+            driver.FindElementByAccessibilityId("textBox_breakfast").SendKeys("нет");
+            driver.FindElementByAccessibilityId("button1").Click();
+
+            WindowsElement errorWindow = driver.FindElementByName("Ошибка");
+            Assert.IsNotNull(errorWindow);
+            var ok = driver.FindElementByName("ОК");
+            ok.Click();
+        }
+
+        [TestMethod]
+        public void TestMethod_category_incorrect()
+        {
+            driver.FindElementByAccessibilityId("textBox_days").SendKeys("1");
+            driver.FindElementByAccessibilityId("textBox_category").SendKeys("10000");
+            driver.FindElementByAccessibilityId("textBox_capacity").SendKeys("3");
+            driver.FindElementByAccessibilityId("textBox_safe").SendKeys("нет");
+            driver.FindElementByAccessibilityId("textBox_breakfast").SendKeys("нет");
+            driver.FindElementByAccessibilityId("button1").Click();
+
+            WindowsElement errorWindow = driver.FindElementByName("Ошибка");
+            Assert.IsNotNull(errorWindow);
+            var ok = driver.FindElementByName("ОК");
+            ok.Click();
+        }
+
+        [TestMethod]
+        public void TestMethod_capacity_incorrect()
+        {
+            driver.FindElementByAccessibilityId("textBox_days").SendKeys("1");
+            driver.FindElementByAccessibilityId("textBox_category").SendKeys("2");
+            driver.FindElementByAccessibilityId("textBox_capacity").SendKeys("0");
+            driver.FindElementByAccessibilityId("textBox_safe").SendKeys("нет");
+            driver.FindElementByAccessibilityId("textBox_breakfast").SendKeys("нет");
+            driver.FindElementByAccessibilityId("button1").Click();
+
+            WindowsElement errorWindow = driver.FindElementByName("Ошибка");
+            Assert.IsNotNull(errorWindow);
+            var ok = driver.FindElementByName("ОК");
+            ok.Click();
+        }
+
+        [TestCleanup]
+        public void Cleanup()
+        {
+            if (driver != null)
+            {
+                try { driver.CloseApp(); } catch { }
+                try { driver.Quit(); } catch { }
+                driver = null;
+            }
+        }
+    }
+}
---
+++ src/Form1.cs
@@ -0,0 +1,52 @@
+namespace HotelCalculator
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                int days = int.Parse(textBox_days.Text);
+                int category = int.Parse(textBox_category.Text);
+                int capacity = int.Parse(textBox_capacity.Text);
+                bool safe = textBox_safe.Text == "да";
+                bool breakfast = textBox_breakfast.Text == "да";
+
+                if ((days <= 0) || (category < 1) || (category > 3) || (capacity < 1) || (capacity > 3))
+                {
+                    MessageBox.Show("Данные введены некорректно", "Ошибка", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                    return;
+                }
+
+                int base_cost = 0;
+                switch (category)
+                {
+                    case 1:
+                        base_cost = 100; break;
+                    case 2:
+                        base_cost = 200; break;
+                    case 3:
+                        base_cost = 300; break;
+                }
+
+
+                int sum = base_cost * capacity * days;
+                if (safe)
+                    sum += 10 * days;
+                if (breakfast)
+                    sum += 20 * capacity * days;
+
+                textBox_sum.Text = sum.ToString();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message, "Ошибка", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+    }
+}
